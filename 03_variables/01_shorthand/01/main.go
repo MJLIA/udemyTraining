6998bb94c2e35275364f21d80b4952ca2f78a753
MJLIA <gepp24@me.com>
2018-01-16 17:55:27 -0500

    moving around completed

---
+++ 03_variables/01_shorthand/01/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  a := 10
+  b := "golang"
+  c := 4.17
+  d := true
+
+  fmt.Printf("%T \t %v \n", a, a)
+  fmt.Printf("%T \t %v \n", b, b)
+  fmt.Printf("%T \t %v \n", c, c)
+  fmt.Printf("%T \t %v \n", d, d)
+
+  }
